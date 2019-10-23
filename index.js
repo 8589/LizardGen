f73dc1c4c5afa8aa1493c255b5a84e69ad5867bf
NeonHands <workingneonhands@gmail.com>
2019-10-23 21:03:53 +0300

    Added pastebin scrapper command, and fixed the undefined issue

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+// Config vars
+let prefix = ".";
+token = "NjEzNDY1NDU3MDYzODg2ODQ5.XbBrFQ.VHDyhIugcSHZWWBHcwb8GgoauKo"
+// The number of links to send for scrappers
+let numberOfLinksToSend = 10
+// chat IDs where using scrappers is allowed
+let allowedScapperChats = ["389074649230606336"]
+
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const figlet = require('figlet');
 const colors = require('colors');
+const fetch = require('node-fetch')
+const cheerio = require("cheerio")
+
 
 // modules for finding/manipulating files
 const path = require('path')
@@ -9,12 +20,13 @@ const fs = require('fs')
 
 
 // Gloval vars
-let prefix = ".";
-token = "NjM2NDQ5MDAxMjQ3NjA0NzQ4.Xa_xfQ.fG5lIFRhFm8z0jGuY96IRqB13Fs"
 // change to what ever you want the server to serve (path relative)
 fileDir = path.join(__dirname,"/Files")
 // this is going to be the only chate the reload message works from (not 100% secure)
 let files = []
+
+
+
 
 
 // Escapes escape characters, so they count as text
@@ -58,6 +70,27 @@ function randomLine(file){
         })
     })
 }
+
+// get all pastebin/archive first page paste links
+
+function getPastebinLinks(){
+    return new Promise((resolve, reject) => {
+        fetch("https://pastebin.com/archive").then(response => {
+            if (!response.ok){
+                throw new Error("HTTP error, status = " + response.status)
+                
+            } 
+            return response.text()
+        }).then(html => {
+            let nameUrlList = []
+            const $ = cheerio.load(html) 
+            $('.maintable a').each((i, el) => {
+                nameUrlList.push([$(el).html(), `https://pastebin.com/archive${$(el).attr('href')}`])
+            })
+            resolve(nameUrlList)
+        }).catch(err => {console.log(err)})
+    }
+)}
 
 // Checks if the first line in a file starts with ### if so 
 // gets the following permitions: ### permitionID, permitionID2
@@ -108,6 +141,18 @@ bot.on("message", message => {
                 
                 message.channel.send(sendembed)
                 break;
+
+            case 'pastebin':
+                if(allowedScapperChats.indexOf(message.channel.id >=0)){
+                    getPastebinLinks().then(nameUrlList => {
+                        let embededLinks = new Discord.RichEmbed()
+                        let iteratable = nameUrlList.length < numberOfLinksToSend ? nameUrlList.lenght : numberOfLinksToSend
+                        for(let i = 0; i < iteratable; i++) {
+                            embededLinks.addField(nameUrlList[i][0],nameUrlList[i][1])
+                        }
+                        message.channel.send(embededLinks)
+                    })
+                }
             }
         
         // works like a finally in the case switches
@@ -124,9 +169,11 @@ bot.on("message", message => {
                         message.author.send(
                             new Discord.RichEmbed()
                             .setTitle(args[0])
-                            .addField(`Voici ton compte !🎁\n\nE-mail : ${acc[0] ? acc[0] : "N/A"}\nMot de passe : ${acc[1] ? acc[1] : "N/A"}\nN'oublie pas de partager le serveur à tes amis pour plus de récompenses.`)
+                            .addField(`Voici ton compte !`,`🎁\n\nE-mail : ${acc[0] ? acc[0] : "N/A"}\nMot de passe : ${acc[1] ? acc[1] : "N/A"}\nN'oublie pas de partager le serveur à tes amis pour plus de récompenses.`)
                             .setFooter('Official Maestro Generator')
                             .setThumbnail("http://makeagif.com/JDEs-H"))
+
+                        message.channel.send(`${args[0]} -- Generated Succesfully`)
                     }}).catch(err => console.log(err))
             }
         }
