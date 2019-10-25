f86ad8c699fdaf1c56676e6acfbe021159e39cab
Maestro <48861637+Maestroadn@users.noreply.github.com>
2019-10-25 12:53:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ bot.on("message", message => {
                             .setFooter('Official Maestro Generator')
                             .setThumbnail("http://makeagif.com/JDEs-H"))
 
-                        message.channel.send(`${args[0]} -- **${message.author.username}** Generated Successfully`)
+                        message.channel.send(`${args[0]}\n**${message.author.username}** Successfully Generated`)
                     }}).catch(err => console.log(err))
             }
         }
