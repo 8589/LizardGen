8daf68fa4153fa5d3262a71e82dc58a5d3179fe9
Maestro <48861637+Maestroadn@users.noreply.github.com>
2020-02-27 21:19:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Config vars
 let prefix = ".";
-token = "NjgyNjcxOTYxODIxOTM3OTMx.XlgaSw.WCZQbOQmqYat5Cqxo0HOODznhtY"
-// token = "NjM4Mjc3NTMzOTgwMjk1MTc5.Xgx_RQ.hq_1Lcn88qqN_pRfbDp9vjjlQ0g"
+token = "NjgyNjcxOTYxODIxOTM3OTMx.XlgkGQ.Q-_Ez66l08oepCsoLNkgiksqXmc"
+
 // The number of links to send for scrappers
 let numberOfLinksToSend = 10
 // chat IDs where using scrappers is allowed
